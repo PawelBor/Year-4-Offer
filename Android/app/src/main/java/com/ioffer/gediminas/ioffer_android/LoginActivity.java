08aafb8895eb4ebe82daba850da5d0cef2d01533
Gediminas <saparauskas@gmail.com>
2017-03-01 11:50:16 +0000

    Created the login page and layouts.

---
+++ Android/app/src/main/java/com/ioffer/gediminas/ioffer_android/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.ioffer.gediminas.ioffer_android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class LoginActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+}
